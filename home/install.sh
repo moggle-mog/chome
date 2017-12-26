535c105cf06a606210291c885d3a7bcd878aed7f
chaslin <35343661@qq.com>
2017-12-26 12:10:47 +0800

    Update install.sh

--- home/install.sh
+++ home/install.sh
@@ -1,31 +1,28 @@
 #! /bin/bash
 mkdir -p $HOME/.dotfiles
 
-git clone --depth=1 git://github.com/joelthelion/autojump.git && \
-cd autojump && \
-./uninstall.py && \
-./install.py && \
-cd ../ && \
-rm -rf autojump
-
+# 修改bashrc文件
 cp -f .bashrc.user $HOME/
-
 echo "[ -f $HOME/.bashrc.user ] && source $HOME/.bashrc.user" >> $HOME/.bashrc
 
+# 安装zsh
 command -v zsh &>/dev/null || \
 ( (command -v dnf &>/dev/null && sudo dnf -y install zsh) || \
 (command -v yum &>/dev/null && sudo yum -y install zsh) || \
 (command -v apt-get &>/dev/null && sudo apt-get install zsh) || \
 (command -v brew &>/dev/null && brew install zsh) || \
 (command "过程被中断,或者使用了不支持的包管理工具" && exit 1) )
-
 export ZSH="$HOME/.dotfiles/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# 修改 zshrc(安装zsh之后会出现)
+sed -i '/^source \$ZSH\/oh-my-zsh.sh/d' .zshrc
 cp -f .zshrc.user $HOME/
 echo "[ -f $HOME/.bashrc.user ] && source $HOME/.bashrc.user" >> $HOME/.zshrc
 echo "[ -f $HOME/.zshrc.user ] && source $HOME/.zshrc.user" >> $HOME/.zshrc
+echo "source \$ZSH/oh-my-zsh.sh" >> $HOME/.zshrc
 
+# 同步更多的配置文件
 cp -f .gitconfig $HOME/
 cp -f .tmux.conf $HOME/
 command -v tmux &>/dev/null || \
