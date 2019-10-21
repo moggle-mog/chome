af48fcd68a04742c2185249b0a28298ae65bf364
chaslin <11258254+moggle-mog@users.noreply.github.com>
2019-10-21 19:23:15 +0800

    Update build.py

--- vim/build.py
+++ vim/build.py
@@ -652,7 +652,7 @@ let g:ycm_confirm_extra_conf = 0
         self._ctags()
         self._cscope()
         self._omnicomplete()
-        self._ycm()
+        #self._ycm()
 
         plugin = ""
         setting = ""
