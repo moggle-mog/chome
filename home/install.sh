a30489c2f1fc79d6b48d14d222ad9517701072e4
chaslin <chaslin@qq.com>
2022-04-04 13:17:11 +0800

    update home bash

--- home/install.sh
+++ home/install.sh
@@ -1,19 +1,49 @@
 #! /bin/bash
 
-function install() {
-    cmd="$1"
-    ((command -v yum &>/dev/null && sudo yum -y install ${cmd}) || \
-    (command -v dnf &>/dev/null && sudo dnf -y install ${cmd}) || \
-    (command -v apt-get &>/dev/null && sudo apt-get install ${cmd}) || \
-    (command -v brew &>/dev/null && brew install ${cmd}) || exit 1 )
-}
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$*${none};  }
+_green() { echo -e ${green}$*${none};  }
+_yellow() { echo -e ${yellow}$*${none};  }
+_magenta() { echo -e ${magenta}$*${none};  }
+_cyan() { echo -e ${cyan}$*${none};  }]']']']']']'
 
-# 安装zsh
-install zsh
-install curl
-install tmux
+# Root
+[[ $(id -u) != 0  ]] && echo -e "\n 请使用 ${red}root ${none}用户运行${yellow}.${none}\n" && exit 1
 
-# 同步更多的配置文件
+# Package
+CMD="yum"
+if [[ $(command -v apt-get) || $(command -v yum) ]] && [[ $(command -v systemctl) ]]; then
+	if [[ $(command -v apt-get) ]]; then
+		CMD="apt-get"
+	fi
+elif [[ $(command -v brew) ]]; then
+	CMD="brew"
+else
+	echo -e " 
+	${red}该脚本${none} 不支持你的系统.${yellow}请确认代码${none}
+	备注: 仅支持 ubuntu 16+ / debian 8+ / centos 7+ / macos 12+ 系统
+	" && exit 1
+fi
+
+# Dependents
+case $CMD in
+'yum')
+   $CMD install -y zsh curl tmux
+   ;;
+'apt-get')
+   $CMD install -y zsh curl tmux
+   ;;
+'brew')
+   $CMD install zsh curl tmux
+   ;;
+esac
+
+# Config
 cp -f .gitconfig $HOME/
 cp -f .tmux.conf $HOME/
 
